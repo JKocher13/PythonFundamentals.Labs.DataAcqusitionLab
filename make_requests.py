497b4b9f030111278a3b8cf3a9c28bf0d64a37b5
JKocher13 <61708469+JKocher13@users.noreply.github.com>
2020-03-25 18:22:45 -0400

    revised function and made some tests

--- make_requests.py
+++ make_requests.py
@@ -5,16 +5,15 @@ import requests
 
 a = 0
 offset_count = 1
-token = INSTERT TOKEN HERE
-while a <= 39:
+token = "ZmqmkkvwvZxdtlLSHXkopRdvSoAJtPWa"
+while a <= 1:
 	name = ("location_" + str(a))
-	url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?location&limit=1000&offset=' +str(offset_count)
+	url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?location&limit=1000&offset=' + str(offset_count)
 	header = {"token": token,
 	            'Content-Type' :  'application/json'}
-	#r = requests.get(url, headers= headers)
 	request = requests.get(url, headers = header)
+	print (request)
 	data = request.json()
-
 	with open ((name + ".json"), 'w') as outfile:
 		json.dump(data, outfile)
 	a = a + 1
